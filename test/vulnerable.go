7b3a633e5b8e24d1718fc37c71b7f2c715c89ba2
dshridhar2000 <57871861+dshridhar2000@users.noreply.github.com>
2026-07-02 17:19:15 +0530

    Update comment from 'test1' to 'test2'

--- test/vulnerable.go
+++ test/vulnerable.go
@@ -17,7 +17,7 @@ func main() {
 	}
 	defer db.Close()
 
-	// test1
+	// test2
 	// Handler that contains multiple vulnerabilities
 	http.HandleFunc("/vulnerable", func(w http.ResponseWriter, r *http.Request) {
 		// Extract untrusted user input from the URL query parameters
