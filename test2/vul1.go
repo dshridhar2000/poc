501ac839351291734556a4c628dcc0113ff13ed3
dshridhar2000 <57871861+dshridhar2000@users.noreply.github.com>
2026-07-05 17:11:53 +0530

    Update vul1.go

--- test2/vul1.go
+++ test2/vul1.go
@@ -1,7 +1,25 @@
 package main
 
-const dbPassword = "SuperSecret123"
+import (
+	"archive/zip"
+	"os"
+	"path/filepath"
+)
 
-func main() {
-	println(dbPassword)
+func unzip(zipFile string) {
+
+	r, _ := zip.OpenReader(zipFile)
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		path := filepath.Join("/tmp/uploads", f.Name)
+
+		out, _ := os.Create(path)
+
+		rc, _ := f.Open()
+
+		defer rc.Close()
+		defer out.Close()
+	}
 }
