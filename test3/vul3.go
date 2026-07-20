d66011d18614acb302beb33f91ffb53eb44ca2ba
dshridhar2000 <57871861+dshridhar2000@users.noreply.github.com>
2026-07-20 19:08:36 +0530

    Update vul3.go

--- test3/vul3.go
+++ test3/vul3.go
@@ -1,19 +1,3 @@
 package main
 
-/*import (
-	"io"
-	"net/http"
-)
 
-func ssrfHandler(w http.ResponseWriter, r *http.Request) {
-
-	url := r.URL.Query().Get("url")
-
-	// Vulnerable
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-
-	w.Write(body)
-}*/
